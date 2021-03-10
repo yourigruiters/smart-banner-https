258289aaf79f93c0cbd31e62c8e8ade716740193
Youri Gruiters <youri.gruiters@ui.com>
2021-03-10 18:25:01 +0100

    Final test

--- src/App.js
+++ src/App.js
@@ -3,32 +3,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
-  // const [prompt, setPrompt] = useState(null);
-
-  // const openPrompt = () => {
-  //   if (!prompt) {
-  //     return;
-  //   }
-
-  //   // Show the prompt
-  //   prompt.prompt();
-
-  //   prompt.userChoice.then((choiceResult) => {
-  //     // console.log(choiceResult) not handling result currently
-  //     setPrompt(null);
-  //   });
-  // };
-  // console.log(openPrompt);
   useEffect(() => {
-    window.addEventListener("beforeinstallprompt", (e) => {
-      console.log("Automatic magic");
-      // setPrompt(e);
-    });
+    window.addEventListener("beforeinstallprompt", () => {});
 
     return () => {
-      window.removeEventListener("beforeinstallprompt", () => {
-        // setPrompt(null);
-      });
+      window.removeEventListener("beforeinstallprompt", () => {});
     };
   }, []);
 
@@ -36,8 +15,7 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Version: 2.02 - Final implementation for automatic banner</p>
-        {/* {prompt && <button onClick={openPrompt}>Open the prompt</button>} */}
+        <p>Version: 2.03 - Automatic banner - No code magic</p>
       </header>
     </div>
   );
