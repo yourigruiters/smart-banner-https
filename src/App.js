d40a2b7d1a8842e57df06b2917ef652561d8bc31
Youri Gruiters <youri.gruiters@ui.com>
2021-03-10 18:03:27 +0100

    Showing banner implementation - 2.0

--- src/App.js
+++ src/App.js
@@ -12,55 +12,30 @@ const App = () => {
 
     // Show the prompt
     prompt.prompt();
-    // Wait for the user to respond to the prompt
+
     prompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === "accepted") {
-        console.log("User accepted the A2HS prompt");
-      } else {
-        console.log("User dismissed the A2HS prompt");
-      }
+      // console.log(choiceResult) not handling result currently
       setPrompt(null);
     });
   };
 
   useEffect(() => {
-    console.log("Listening for beforeinstallprompt...");
     window.addEventListener("beforeinstallprompt", (e) => {
-      console.log("Heard beforeinstallprompt, trying to setup..");
       setPrompt(e);
     });
 
     return () => {
       window.removeEventListener("beforeinstallprompt", () => {
-        console.log("Removed eventlistener");
+        setPrompt(null);
       });
     };
   }, []);
-
-  useEffect(() => {
-    if (prompt) {
-      alert("showing...");
-      // Show the prompt
-      prompt.prompt();
-      // Wait for the user to respond to the prompt
-      prompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === "accepted") {
-          console.log("User accepted the A2HS prompt");
-        } else {
-          console.log("User dismissed the A2HS prompt");
-        }
-        setPrompt(null);
-      });
-    } else {
-      alert("en doooor...");
-    }
-  }, [prompt]);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Version: 1.06 - Auto open prompt</p>
+        <p>Version: 2.01 - Final implementation for showing banner</p>
         {prompt && <button onClick={openPrompt}>Open the prompt</button>}
       </header>
     </div>
